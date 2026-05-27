151c29bc454a09407aeb45026f106cbf5c9ea50d
Sev3nB <sev3nb@gmail.com>
2026-05-27 20:59:48 +0200

    Aggiunto tutto

---
+++ app.py
@@ -0,0 +1,41 @@
+import json
+from flask import Flask,render_template, abort
+
+app = Flask(__name__)
+def carica_dati(jsonFile):
+    try:
+        with open(jsonFile, 'r', encoding='utf-8') as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return []
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route('/progetti')
+def progetti():
+    sezioni_pagina = carica_dati("progetti.json")
+    return render_template('progetti.html', sezioni=sezioni_pagina)
+
+@app.route('/competizioni')
+def competizioni():
+    competizioni = carica_dati("competizioni.json")
+    return render_template('competizioni.html', competizioni=competizioni)
+
+@app.route('/competizione/<int:comp_id>')
+def dettaglio(comp_id):
+    competizioni = carica_dati()
+    gara = next((item for item in competizioni if item["id"] == comp_id), None)
+    
+    if gara is None:
+        abort(404)
+        
+    return render_template('dettaglio_comp.html', gara=gara)
+
+@app.route("/contatti")
+def contatti():
+    return render_template("contatti.html")
+
+if (__name__ == "__main__"):
+    app.run(debug=True)
